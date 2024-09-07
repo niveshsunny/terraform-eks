a62cda85cdc8573dc4afc26619ac2a1c7b4007d0
niveshsunny <niveshreddy@gmail.com>
2024-09-07 14:06:45 -0400

    outputs update

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,43 @@ output "public_instance_id" {
 output "private_instance_id" {
   value = aws_instance.private_instance.id
 }
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+
+output "eks_cluster_version" {
+  value = aws_eks_cluster.eks.version
+}
+
+output "eks_cluster_arn" {
+  value = aws_eks_cluster.eks.arn
+}
+
+output "eks_node_group_name" {
+  value = aws_eks_node_group.node-grp.node_group_name
+}
+
+output "eks_node_group_arn" {
+  value = aws_eks_node_group.node-grp.arn
+}
+
+output "eks_node_group_instance_types" {
+  value = aws_eks_node_group.node-grp.instance_types
+}
+
+output "eks_node_group_desired_size" {
+  value = aws_eks_node_group.node-grp.scaling_config[0].desired_size
+}
+
+output "eks_node_group_max_size" {
+  value = aws_eks_node_group.node-grp.scaling_config[0].max_size
+}
+
+output "eks_node_group_min_size" {
+  value = aws_eks_node_group.node-grp.scaling_config[0].min_size
+}
