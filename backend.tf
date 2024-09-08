56631e4b805ea0e0a5dd051dfb335f8a528c27a1
niveshsunny <niveshreddy@gmail.com>
2024-09-07 21:34:34 -0400

    added dynamo db

--- backend.tf
+++ backend.tf
@@ -5,3 +5,14 @@ terraform {
     key = "nivesh/terraform.tfstate"
   }
 }
+
+# resource "aws_dynamodb_table" "terraform_lock" {
+#   name           = "terraform-lock"
+#   billing_mode   = "PAY_PER_REQUEST"
+#   hash_key       = "LockID"
+
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
